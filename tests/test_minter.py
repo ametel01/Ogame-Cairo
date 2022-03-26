d25365d4459abd21b41a07e379fd8bb1845eb636
ametel01 <alex-metelli@gmx.com>
2022-03-26 18:02:14 +0000

    refactored minter test

--- tests/test_minter.py
+++ tests/test_minter.py
@@ -1,22 +1,20 @@
 import pytest
 from utils.helpers import assert_equals
-from starkware.starknet.compiler.compile import get_selector_from_name
 from conftest import owner
 
 
 @pytest.mark.asyncio
-async def test_mint_NFTs(deploy_game_v1, minter_factory):
-    (ogame, erc721, _, _, _, admin, _) = deploy_game_v1
-    minter = minter_factory
+async def test_mint_NFTs(deploy_game_v1, admin):
+    (minter, ogame, erc721, _, _, _, _) = deploy_game_v1
 
-    # Assert minte contract NFT balance is equal 199.
-    data = await admin.execute(1, erc721.contract_address,
-                               get_selector_from_name('balanceOf'),
-                               [minter.contract_address], 4).invoke()
+    # Assert mint contract NFT balance is equal 199.
+    data = await owner.send_transaction(admin, erc721.contract_address,
+                                        'balanceOf',
+                                        [minter.contract_address])
     assert_equals(data.result.response[0], 199)
 
     # Assert that the game received approval.
-    data = await admin.execute(erc721.contract_address,
-                               get_selector_from_name('isApprovedForAll'),
-                               [minter.contract_address, ogame.contract_address], 5).invoke()
+    data = await owner.send_transaction(admin, erc721.contract_address,
+                                        'isApprovedForAll',
+                                        [minter.contract_address, ogame.contract_address])
     assert_equals(data.result.response[0], 1)
